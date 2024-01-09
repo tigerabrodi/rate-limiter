ffc9428ec19a3ced2b385e5ab1e15366a8ca8ad4
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-09 07:23:03 +0100

    setup server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello, world!')
+import express from 'express'
+
+const app = express()
+const port = 8080
+
+app.get('/unlimited', (req, res) => {
+  res.send("Unlimited! Let's Go!")
+})
+
+app.get('/limited', (req, res) => {
+  res.send("Limited, don't overuse me!")
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`)
+})
